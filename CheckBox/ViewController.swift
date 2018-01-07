9aa96af71ffe8f8e70f3660e38e60f8c4e967f7d
penumutchu.prasad@gmail.com <penumutchu.prasadgmail.com@penumutchu-macbook-pro.local>
2018-01-07 18:15:58 +0530

    Initial Commit

---
+++ CheckBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckBox
+//
+//  Created by penumutchu.prasad@gmail.com on 07/01/18.
+//  Copyright © 2018 abnboys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
